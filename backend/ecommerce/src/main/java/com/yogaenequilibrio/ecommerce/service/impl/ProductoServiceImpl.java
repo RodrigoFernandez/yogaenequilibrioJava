93699a69262f157c279f49315ea8611a9f8120c1
Rodrigo Fernandez <rdr.rkh+github2@gmail.com>
2026-06-23 21:48:59 -0300

    Actualización del método de actualización de productos.

--- backend/ecommerce/src/main/java/com/yogaenequilibrio/ecommerce/service/impl/ProductoServiceImpl.java
+++ backend/ecommerce/src/main/java/com/yogaenequilibrio/ecommerce/service/impl/ProductoServiceImpl.java
@@ -31,11 +31,40 @@ public class ProductoServiceImpl implements ProductoService {
         return productoRepository.save(producto);
     }
 
+    private Producto actualizarProductoExistente(Producto existente, Producto nuevo) {
+        // Actualiza los campos del producto existente con los valores del nuevo producto
+        if(nuevo.getNombre() != null) {
+            existente.setNombre(nuevo.getNombre());
+        }
+
+        if(nuevo.getDescripcion() != null) {
+            existente.setDescripcion(nuevo.getDescripcion());
+        }
+        
+        if(nuevo.getPrecio() != null) {
+            existente.setPrecio(nuevo.getPrecio());
+        }
+        
+        if(nuevo.getImagen() != null) {
+            existente.setImagen(nuevo.getImagen());
+        }
+        
+        if(nuevo.getEsNovedad() != null) {
+            existente.setEsNovedad(nuevo.getEsNovedad());
+        }
+
+        if(nuevo.getEsDestacado() != null) {
+            existente.setEsDestacado(nuevo.getEsDestacado());
+        }
+
+        return existente;
+    }
+
     @Override
     public Optional<Producto> actualizar(Integer id, Producto producto) {
         if (productoRepository.existsById(id)) {
-            producto.setId(id);
-            return Optional.of(productoRepository.save(producto));
+            Producto actualizado = this.actualizarProductoExistente(productoRepository.findById(id).get(), producto);
+            return Optional.of(productoRepository.save(actualizado));
         }
         
         return Optional.empty();
